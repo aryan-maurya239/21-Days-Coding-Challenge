7a1e8f037f73ae68dc13cd02346845ebcfe8ced3
aryan-maurya239 <mauryaaryan239@gmail.com>
2024-08-26 12:13:06 +0530

    2 day of 21 days coding challenge
    These days I am learning
    If---else in one line,
    Enumerate function,
    virtual Environment,
    importing modules,
    from keywords,
    as keywords,
    dir function

---
+++ Enumerate.py
@@ -0,0 +1,11 @@
+marks=[10,15,45,99,45,54,54]
+for index, i in enumerate(marks):
+    print(index,i )
+    if(index == 3):
+        print("     ,Aryan, awaysom good number")
+        
+        
+print("\n")
+for index ,i in enumerate(marks,start=1):
+    print(index,i)
+    
