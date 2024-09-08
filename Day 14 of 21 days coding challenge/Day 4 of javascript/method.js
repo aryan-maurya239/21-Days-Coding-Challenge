3f9eef4f2fe342e664c6a877bbded7a50fc2310c
aryan-maurya239 <mauryaaryan239@gmail.com>
2024-09-08 14:50:15 +0530

    Day 14 of 21 - Coding Challenge 🚀
    Today marks Day 14 of my 21-day coding challenge!
    💻 I'm continuing to push forward, and today I focused on:
    🔹 Python Class Methods – Gained a deeper understanding of how to use class methods and when to choose them over instance or static methods.🐍💡
    🔹 CSS Flexbox – Practiced with align-items, align-content, align-self, flex-sizing, and flex-shorthand. 🎨 Flexbox makes layout design so much more flexible and responsive! 🔄
    🔹 JavaScript Arrays & Methods – Learned more about arrays, their mutability, and how to manipulate them using methods like indexOf() and includes(). Also refreshed my knowledge on string methods like slice() and replace().
     📊🔢Excited to keep learning and growing. Just one more week to go!
    
    💪🔥#21DayChallenge #Python #JavaScript #CSS #Flexbox #CodingJourney #WebDevelopment #LearningEveryday #Tech

---
+++ Day 14 of 21 days coding challenge/Day 4 of javascript/method.js
@@ -0,0 +1,39 @@
+let str = "IloveCoding";
+str.slice(5)
+str.slice(1,4)
+str.slice(-6)
+console.log(str.replace('love','Do'))
+console.log(str.replace('o','D'))
+// onle first o is replace with d 
+// out put : IlDveCoding
+
+console.log(str.repeat(5))
+
+let msg = 'help!';
+console.log(msg.trim().toUpperCase())
+
+let name = "MeraName Aryan"
+console.log(name.slice(4,9))
+console.log(name.indexOf('Ar'))
+console.log(name.replace('Aryan','Manish'))
+console.log(name.repeat(3))
+console.log(name.slice(9,length.name).replace("Aryan",'Vinit'))
+
+
+let array = ['Aryan',234,24.4,"cartun"]
+console.log(array[0][4])
+array[0] = 'Sachin' 
+console.log(array[0])
+console.log(array)
+array[10] = 34
+console.log(array)
+array.push('sonal')
+console.log(array.pop())
+array.unshift('Ram')
+array.shift()
+
+month = ['january','july', 'march', 'august'];
+console.log(month);
+
+month[1] = 'june'
+console.log(month);
